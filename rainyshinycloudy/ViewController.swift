c86cffcd24a0a9c8aaa429b98d010f657b4876a5
Quang Nguyen <qilytquang@gmail.com>
2016-12-14 14:07:51 +0700

    completed install alamofire using Cocoapods, creating UIs, making tableView work

---
+++ rainyshinycloudy/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  WeatherVC.swift
+//  rainyshinycloudy
+//
+//  Created by quang nguyen on 12/13/16.
+//  Copyright © 2016 quang nguyen. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+}
+
